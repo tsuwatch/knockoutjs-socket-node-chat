cc0e2b921146ec21295bb8611d37b4e4ce41139f
Tomohiro Suwa <neoen.gsn@gmail.com>
2014-09-25 20:43:29 +0900

    Change title to knockoutjs-socket-node-chat

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'chatApp' });
+  res.render('index', { title: 'knockoutjs-socket-node-chat' });
 });
 
 module.exports = router;
